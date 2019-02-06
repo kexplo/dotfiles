5de3b593ecae879a318af94d8cd755867cda3afd
Chanwoong Kim <me@chanwoong.kim>
2019-02-06 14:59:56 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+random_str() {
+  # reference: https://stackoverflow.com/a/2793856/1545387
+  printf "%s" "$(env LC_CTYPE=C tr -cd 'a-f0-9' < /dev/urandom | head -c 4)"
+}
+
+is_osx() {
+ [[ "$(uname)" == "Darwin" ]]
+}
+
+_readlink() {
+  if is_osx; then
+    greadlink "$@"
+  else
+    readlink "$@"
+  fi
+}
+
+check_dependency() {
+  if ! is_osx; then
+    return
+  fi
+  if ! which greadlink > /dev/null; then
+    echo "Can't found greadlink, you need to run 'brew install coreutils'"
+    exit 1
+  fi
+}
+
+sym_link() {
+  local src="$1"
+  local dest="$2"
+
+  if [[ -b "$dest" || -e "$dest" || -L "$dest" ]]; then
+    if [[ "$(_readlink -f "$src")" == "$(_readlink -f "$dest")" ]]; then
+      return
+    fi
+    backup_name="${dest}.$(random_str).bak"
+    echo "Backup existing file(or directory) '$dest' to '$backup_name'"
+    mv "$dest" "$backup_name"
+  fi
+  ln -s "$src" "$dest"
+}
+
+RC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+check_dependency
+
+sym_link "$RC_DIR/stdlib.sh" "$HOME/stdlib.sh"
+sym_link "$RC_DIR/.vimrc" "$HOME/.vimrc"
+sym_link "$RC_DIR/.zshrc" "$HOME/.zshrc"
+sym_link "$RC_DIR/.gitconfig" "$HOME/.gitconfig"
+sym_link "$RC_DIR/.tmux.conf" "$HOME/.tmux.conf"
+sym_link "$RC_DIR/.ptpython" "$HOME/.ptpython"
